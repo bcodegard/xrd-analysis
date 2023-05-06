5994c9a441909d4d6ed4c929620f5259db923500
bcodegard <bcodegard@gmail.com>
2023-05-05 18:22:49 -0700

    Basic examples of some new UI functionality
    In dev script, add some examples of using QT widgets, and picking artists by clicking.

--- test-utils-interact.py
+++ test-utils-interact.py
@@ -6,10 +6,15 @@ don't actually use this for anything else
 import numpy
 import math
 import sys
+
 import numpy as np
+
+from PyQt6 import QtGui, QtCore
+import PyQt6.QtWidgets as Widgets
 
 import utils.interact    as interact
 import matplotlib.pyplot as plt
+
 
 
 M_KEY_PRESS_BRIEF   = "press   {}" 
@@ -31,7 +36,7 @@ def release(press, release):
 	print("press   : {}".format(press))
 	print("release : {}".format(release))
 
-	release.inaxes.axvspan(
+	vspan = release.inaxes.axvspan(
 		press.xdata,
 		release.xdata,
 
@@ -42,6 +47,9 @@ def release(press, release):
 		# alpha = 0.15,
 	)
 
+	# make pickable
+	vspan.set_picker(True)
+
 	release.canvas.draw()
 
 
@@ -51,7 +59,7 @@ if __name__ == "__main__":
 
 	xaxis = np.linspace(10, 350, 200)
 	
-	ydist = 15.0 + xaxis*0.075 + 35 / (1 + ((xaxis - 175) / 12)**2)
+	ydist = 15.0 + xaxis*0.015 + 15 / (1 + ((xaxis - 175) / 21)**2)
 	ydata = np.random.poisson(ydist)
 
 
@@ -67,8 +75,41 @@ if __name__ == "__main__":
 
 	fi.bind_button_release(1, release, "alt")
 
-	ax.step(xaxis, ydata, 'k-' , where='mid', label='generated sample')
-	ax.step(xaxis, ydist, 'r--', where='mid', label='true distribution')
+
+	# qt stuff!?
+	toolbar = fig.canvas.toolbar
+	
+	line_edit = Widgets.QLineEdit()
+	fig.canvas.toolbar.addWidget(line_edit)
+	line_edit.setStyleSheet("background-color: #101040")
+	line_edit.editingFinished.connect(lambda *args:print(args))
+
+	# tree = Widgets.QTreeWidget()
+	# fig.canvas.toolbar.addWidget(tree)
+	# tree.setStyleSheet("background-color: #606080")
+	# tree.addTopLevelItem(Widgets.QTreeWidgetItem(("what", "the")))
+
+
+	# pickable artists!
+	# 
+	# In theory, could have tree view widget that gives access data of
+	# any picked artist, and allows for editing.
+	# Could have binding for selecting artist to edit, E.G. alt + left
+	# 
+	# different artists could also have individual functionality,
+	# for example click to move vlines, vspans, legend, etc.
+	# Maybe also a key combo for moving, E.G. (shift + left) drag & drop
+	# 
+	# key combo for removing
+	# 
+	s1, = ax.step(xaxis, ydata, 'k-' , where='mid', label='generated sample')
+	s2, = ax.step(xaxis, ydist, 'r--', where='mid', label='true distribution')
+	s1.set_picker(True)
+	s2.set_picker(True)
+	fig.canvas.mpl_connect('pick_event', lambda event: print(event.artist))
+
+
+	# label and show
 	ax.set_xlabel("x axis")
 	ax.set_ylabel("counts")
 	plt.legend()
